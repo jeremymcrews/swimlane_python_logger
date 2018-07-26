ec71c4122e9c80fd986949d4ad224a5d1c948331
Jeremy Crews <jeremyc@phoenixnap.com>
2018-07-26 14:45:34 -0700

    New Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swimlane-python-logger",
-    version="1.0.0",
+    version="1.0.1",
     author="Jeremy m Crews",
     author_email="jeremy.m.crews@gmail.com",
     description="Logger wrapper class for Swimlane Integrations",
